d5da29b2ad29bef00f43a7d8ad51fcdbac2c4c61
Mark Luoma <luomamr@gmail.com>
2019-02-12 11:57:45 -0500

    changed package

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 let mysql = require("mysql");
 require('dotenv').config()
 
+let connection;
 
-let connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.DB_PASS,
   database: "burgers_db"
 });
-
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
